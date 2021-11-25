d08dccd728da768bfdda6344ba6b9339c72b9c6b
Achrefgasri2 <gasriachref@gmail.com>
2021-11-25 11:39:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { Button } from 'react-bootstrap'
+import Comp from './component/component.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+      Person : {
+          fullName: "Achref Gasri",
+          Profession: "Full Stack Developer",
+  
+          imgSrc: "/images/ProfilePhoto.jpg",
+          Bio: {
+              diploma: ["bachelor's degree in economics and management", "/Applied Bachelor of Computer Science"],
+              skills: ["HTML5/", "CSS3/", "JS/", "PHP", "/SQL", "/ReactJS", "/Monogodb", "/Symfony 5", "/XML", "/Oracle", "/JAVA", "/Github"],
+              lang: { Arabic: "excellent", English: "middle", French: "good" }
+          }
+      },
+
+    show: false,
+  }
+  handleShow = () => {
+    this.setState({
+      show: !this.state.show,
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Button  variant="danger" onClick={this.handleShow} style={{ marginBottom: "10%" }}>{this.state.show ? "Hide" : "Show"}</Button>
+        {this.state.show &&
+          <Comp Person={this.state.Person} />
+        }
+      </div>
+
+    )
+  }
 }
 
-export default App;
---
+++ src/component/component.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import css from './component.css'
+
+export default class Comp extends Component {
+
+    state = {
+        count: 0
+    }
+
+    componentDidMount = () => {
+        setInterval(() => {
+            this.setState({ count: this.state.count + 1 })
+        }, 1000);
+    }
+
+
+    render() {
+        return (
+            <div>
+
+                <h2>{this.state.count}</h2>
+                <img src={this.props.Person.imgSrc} width={320} alt="ProfilePhoto" />
+                <h2> FullName :{this.props.Person.fullName} </h2>
+                <h2> Profession: {this.props.Person.Profession} </h2>
+                <h2> Diploma :{this.props.Person.Bio.diploma} </h2>
+                <h2> Skills : {this.props.Person.Bio.skills} </h2>
+                <h2>Arabic : {this.props.Person.Bio.lang.Arabic} </h2>
+                <h2> English : {this.props.Person.Bio.lang.French} </h2>
+                <h2> French : {this.props.Person.Bio.lang.English} </h2>
+            </div>
+
+        )
+    }
+}
+
